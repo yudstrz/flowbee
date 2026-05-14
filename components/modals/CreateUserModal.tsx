044d326092b742c3eba2139e4acb6a70f8ecd60a
yud! <145757584+yudstrz@users.noreply.github.com>
2026-05-15 04:38:21 +0700

    fix: import missing useEffect in user modals to resolve build errors

--- components/modals/CreateUserModal.tsx
+++ components/modals/CreateUserModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HP_TOKENS, HP_FONT, HP_TEXT } from "@/lib/constants";
 import Modal from "@/components/ui/Modal";
 import HPGlyph from "@/components/ui/HPGlyph";
